1ba32c187505ac2ede97fe802ef06947d69e6047
cyhoo <cyhoo0528@gmail.com>
2024-09-01 23:16:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c40mb37wdlec.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "hoochunyuan-employee"
+customregion = "us-east-1"
+
